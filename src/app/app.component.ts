75214e3da2bc2e5d3a5c6c5102af067d50670d5a
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2022-10-07 13:38:31 +0530

    weather is showing properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testapp';
+  email = "Email Address";
+  msg1 = "We'll never share your email with anyone else.";
+  password = "Password";
+  msg2 = "Check me out";
+  submit = "Submit";
+  currUrl = window.location.href;
 }
